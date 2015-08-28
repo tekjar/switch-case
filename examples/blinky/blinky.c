125744ca0d0d216db84f4eba6eecdd11a5404e1e
RTR <k.teza1@gmail.com>
2015-08-28 12:07:02 +0530

    Code alignment

--- examples/blinky/blinky.c
+++ examples/blinky/blinky.c
@@ -11,17 +11,17 @@
 #define BLUE 1
 
 #ifdef RED
-	#define pin GPIO_ID_PIN(12)
-	#define pin_func FUNC_GPIO12
-	#define pin_mux PERIPHS_IO_MUX_MTDI_U
+    #define pin GPIO_ID_PIN(12)
+    #define pin_func FUNC_GPIO12
+    #define pin_mux PERIPHS_IO_MUX_MTDI_U
 #elif BLUE
-	#define pin GPIO_ID_PIN(13)
-	#define pin_func FUNC_GPIO13
-	#define pin_mux PERIPHS_IO_MUX_MTCK_U
+    #define pin GPIO_ID_PIN(13)
+    #define pin_func FUNC_GPIO13
+    #define pin_mux PERIPHS_IO_MUX_MTCK_U
 #elif GREEN
-	#define pin GPIO_ID_PIN(14)
-	#define pin_func FUNC_GPIO14
-	#define pin_mux PERIPHS_IO_MUX_MTMS_U
+    #define pin GPIO_ID_PIN(14)
+    #define pin_func FUNC_GPIO14
+    #define pin_mux PERIPHS_IO_MUX_MTMS_U
 #endif
 
 #define LOW 0
@@ -35,46 +35,44 @@ static volatile os_timer_t some_timer;
 void some_timer_func(void *arg) // in Arduino this is loop the main loop
 {
     //Do blinky stuff
-	if GPIO_INPUT_GET(pin)  
+    if GPIO_INPUT_GET(pin)  
     {
         //Set led to LOW
-		GPIO_OUTPUT_SET(pin, LOW);
-		os_printf("ON \n\r");      // In Arduino this is Serial.println("ON");
+        GPIO_OUTPUT_SET(pin, LOW);
+        os_printf("ON \n\r");      // In Arduino this is Serial.println("ON");
 
     }
     else
     {
         //Set led to HIGH
-		GPIO_OUTPUT_SET(pin, HIGH);
-		os_printf("OFF \n\r");      // In Arduino this is Serial.println("OFF");
+        GPIO_OUTPUT_SET(pin, HIGH);
+        os_printf("OFF \n\r");      // In Arduino this is Serial.println("OFF");
 
     }
 }
 
 
-//Init function 
-void ICACHE_FLASH_ATTR
-user_init()  // in arduino this is setup()
+void ICACHE_FLASH_ATTR user_init()  // in arduino this is setup()
 {
     // Initialize the GPIO subsystem.
     gpio_init();
 
- 	// config pin as normal GPIO
- 	PIN_FUNC_SELECT(pin_mux,pin_func);
+    // config pin as normal GPIO
+    PIN_FUNC_SELECT(pin_mux,pin_func);
 
-	//Set GPIO to output mode, and to low
+    //Set GPIO to output mode, and to low
     GPIO_OUTPUT_SET(pin, 1);
 
-	
+
     // Initialize UART0
-	uart_div_modify(0, UART_CLK_FREQ / 115200);  // In Arduino this is Serial.begin(115200);
+    uart_div_modify(0, UART_CLK_FREQ / 115200);  // In Arduino this is Serial.begin(115200);
 
-	os_printf("Hello World, Blinking\n\r"); // In Arduino this is Serial.println("Hello World, Blinking");
+    os_printf("Hello World, Blinking\n\r"); // In Arduino this is Serial.println("Hello World, Blinking");
 
-	// In Arduino there is 1 loop, Under ESP8266 you can have several loops next to each other. 
-	// To setup a loop use the 3 timer commands below. This will start the function called some_timer
-	// some_timer will be repeated every second.
-	
+    // In Arduino there is 1 loop, Under ESP8266 you can have several loops next to each other. 
+    // To setup a loop use the 3 timer commands below. This will start the function called some_timer
+    // some_timer will be repeated every second.
+
     //Disarm timer
     os_timer_disarm(&some_timer);
 
@@ -86,5 +84,5 @@ user_init()  // in arduino this is setup()
     //1000 is the fire time in ms
     //0 for once and 1 for repeating
     os_timer_arm(&some_timer, 1000, 1);
-    
+
 }
